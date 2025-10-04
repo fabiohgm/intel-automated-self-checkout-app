f266da98cc18a06765e07a806f9c6d9f19abeb6a
Fabio Henrique <176734033+fabiohgm@users.noreply.github.com>
2025-10-04 02:30:43 -0300

    Add initial backend, frontend, pipeline configuration, and gitignore files

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Intel Automated Self Checkout Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
